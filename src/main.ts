3f253d69343ba3083442cd5db7c919c95e8e7c3c
zblzbl1991 <357760876@qq.com>
2021-07-18 22:42:27 +0800

    增加京东侧边栏样式学习

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
-
+import 'amfe-flexible/index.js'
 const app = createApp(App)
 installElementPlus(app)
 app.mount('#app')
